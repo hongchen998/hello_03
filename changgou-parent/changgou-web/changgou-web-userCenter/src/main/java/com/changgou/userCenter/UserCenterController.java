384925cc9d621118a28fe901b8980d5c4a9a35da
hongchen <hongchen998@foxmail.com>
2020-04-28 17:24:06 +0800

    海龙提交本地。 完成用户中心首页展示

---
+++ changgou-parent/changgou-web/changgou-web-userCenter/src/main/java/com/changgou/userCenter/UserCenterController.java
@@ -0,0 +1,24 @@
+package com.changgou.userCenter;/**
+ * @author hongchen
+ * @date 2020/4/28 16:34
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *@ClassName UserCenterController
+ *@Description 用户中心视图解析
+ *@Author hongchen
+ *@Date 16:34 2020/4/28
+ *@Version 2.1
+ **/
+@Controller
+@RequestMapping("/userCenter")
+public class UserCenterController {
+
+    @RequestMapping("/home")
+    public String userCenter(){
+        return "home";
+    }
+}
